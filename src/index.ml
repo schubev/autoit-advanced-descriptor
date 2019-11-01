7f20a83fd360857043c01787501877b23389c4ec
Victor Schubert <v@schu.be>
2019-11-01 16:03:23 +0100

    make compiled javascript dependency-free

--- src/index.ml
+++ src/index.ml
@@ -12,79 +12,60 @@ type windowDescription =
   ; instance: int Js.Nullable.t }
 [@@bs.deriving abstract]
 
-type windowDescriptor =
-  | Title of string
-  | ClassName of string
-  | RegexpTitle of string
-  | RegexpClassName of string
-  | Last of bool
-  | Active of bool
-  | X of int
-  | Y of int
-  | W of int
-  | H of int
-  | Instance of int
-
 let escaped s = s |. Js.String2.replaceByRe [%re "/;/g"] ";;"
 
-let stringOfWindowDescriptor = function
-  | Title title ->
-      Some ("TITLE:" ^ escaped title)
-  | ClassName className ->
-      Some ("CLASS:" ^ escaped className)
-  | RegexpTitle regexpTitle ->
-      Some ("REGEXPTITLE:" ^ escaped regexpTitle)
-  | RegexpClassName regexpClassName ->
-      Some ("REGEXPCLASS:" ^ escaped regexpClassName)
-  | Last last ->
-      if last then Some "LAST" else None
-  | Active active ->
-      if active then Some "ACTIVE" else None
-  | X x ->
-      Some ("X:" ^ string_of_int x)
-  | Y y ->
-      Some ("Y:" ^ string_of_int y)
-  | W w ->
-      Some ("W:" ^ string_of_int w)
-  | H h ->
-      Some ("H:" ^ string_of_int h)
-  | Instance instance ->
-      Some ("INSTANCE:" ^ string_of_int instance)
+let addStringDescriptor acc key value =
+  let value = Js.Nullable.toOption value in
+  match value with
+  | Some value ->
+      ignore @@ Js.Array2.push acc (key ^ ":" ^ escaped value)
+  | None ->
+      ()
 
-let optionMap f = function Some x -> Some (f x) | None -> None
+let addIntegerDescriptor acc key value =
+  let value = Js.Nullable.toOption value in
+  match value with
+  | Some value ->
+      ignore @@ Js.Array2.push acc (key ^ ":" ^ string_of_int @@ value)
+  | None ->
+      ()
 
-let filterSome xs =
-  xs |. Js.Array2.filter Js.Option.isSome |. Js.Array2.map Js.Option.getExn
+let addBooleanDescriptor acc key value =
+  let value = Js.Nullable.toOption value in
+  match value with
+  | Some true ->
+      ignore @@ Js.Array2.push acc key
+  | Some false | None ->
+      ()
 
-let splitDescription windowDescription =
-  [| windowDescription |. titleGet |. Js.Nullable.toOption
-     |> optionMap (fun title -> Title title)
-   ; windowDescription |. classNameGet |. Js.Nullable.toOption
-     |> optionMap (fun className -> ClassName className)
-   ; windowDescription |. regexpTitleGet |. Js.Nullable.toOption
-     |> optionMap (fun regexpTitle -> RegexpTitle regexpTitle)
-   ; windowDescription |. regexpClassNameGet |. Js.Nullable.toOption
-     |> optionMap (fun regexpClassName -> RegexpClassName regexpClassName)
-   ; windowDescription |. lastGet |. Js.Nullable.toOption
-     |> optionMap (fun last -> Last last)
-   ; windowDescription |. activeGet |. Js.Nullable.toOption
-     |> optionMap (fun active -> Active active)
-   ; windowDescription |. xGet |. Js.Nullable.toOption
-     |> optionMap (fun x -> X x)
-   ; windowDescription |. yGet |. Js.Nullable.toOption
-     |> optionMap (fun y -> Y y)
-   ; windowDescription |. wGet |. Js.Nullable.toOption
-     |> optionMap (fun w -> W w)
-   ; windowDescription |. hGet |. Js.Nullable.toOption
-     |> optionMap (fun h -> H h)
-   ; windowDescription |. instanceGet |. Js.Nullable.toOption
-     |> optionMap (fun instance -> Instance instance) |]
-  |. filterSome
+let descriptorTagged ~title ~className ~regexpTitle ~regexpClassName ~last
+    ~active ~x ~y ~w ~h ~instance () =
+  let descriptors = [||] in
+  addStringDescriptor descriptors "TITLE" title ;
+  addStringDescriptor descriptors "CLASS" className ;
+  addStringDescriptor descriptors "REGEXPTITLE" regexpTitle ;
+  addStringDescriptor descriptors "REGEXPCLASS" regexpClassName ;
+  addIntegerDescriptor descriptors "INSTANCE" instance ;
+  addBooleanDescriptor descriptors "ACTIVE" active ;
+  addBooleanDescriptor descriptors "LAST" last ;
+  addIntegerDescriptor descriptors "X" x ;
+  addIntegerDescriptor descriptors "Y" y ;
+  addIntegerDescriptor descriptors "W" w ;
+  addIntegerDescriptor descriptors "H" h ;
+  let body = descriptors |. Js.Array2.joinWith "; " in
+  "[" |. Js.String2.concatMany [|body; "]"|]
 
 let descriptor windowDescription =
-  let descriptors = splitDescription windowDescription in
-  "["
-  ^ ( descriptors
-    |. Js.Array2.map stringOfWindowDescriptor
-    |. filterSome |. Js.Array2.joinWith "; " )
-  ^ "]"
+  let title = windowDescription |. titleGet in
+  let className = windowDescription |. classNameGet in
+  let regexpTitle = windowDescription |. regexpTitleGet in
+  let regexpClassName = windowDescription |. regexpClassNameGet in
+  let instance = windowDescription |. instanceGet in
+  let active = windowDescription |. activeGet in
+  let last = windowDescription |. lastGet in
+  let x = windowDescription |. xGet in
+  let y = windowDescription |. yGet in
+  let w = windowDescription |. wGet in
+  let h = windowDescription |. hGet in
+  descriptorTagged ~title ~className ~regexpTitle ~regexpClassName ~instance
+    ~active ~last ~x ~y ~w ~h ()
