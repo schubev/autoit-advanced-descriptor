4210fe44a6d1540f12fd8e07f1aa1b9fe1a77f39
Victor Schubert <v@schu.be>
2020-01-10 16:35:25 +0100

    add typescript typings

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+export function descriptor(windowDescription: {
+  title?: string
+  className?: string
+  regexpTitle?: string
+  regexpClassName?: string
+  instance?: number
+  active?: boolean
+  last?: boolean
+  x?: number
+  y?: number
+  w?: number
+  h?: number
+}): string
