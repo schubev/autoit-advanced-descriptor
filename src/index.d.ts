3f1978e7486e0953a90355b97afb9343d3459b05
Victor Schubert <v@schu.be>
2020-01-22 18:36:18 +0100

    improve typescript interface

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,15 @@
-export type WindowDescription = {
-  title?: string
-  className?: string
-  regexpTitle?: string
-  regexpClassName?: string
-  instance?: number
-  active?: boolean
-  last?: boolean
-  x?: number
-  y?: number
-  w?: number
-  h?: number
+export interface WindowDescription {
+  readonly title?: string
+  readonly className?: string
+  readonly regexpTitle?: string
+  readonly regexpClassName?: string
+  readonly instance?: number
+  readonly active?: boolean
+  readonly last?: boolean
+  readonly x?: number
+  readonly y?: number
+  readonly w?: number
+  readonly h?: number
 }
 
 export function descriptor(windowDescription: WindowDescription): string
